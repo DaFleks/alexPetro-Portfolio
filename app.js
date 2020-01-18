f477cfc2155b2c70651153e77b6c9d79b4f3e038
DaFleks <petropoulosalex@gmail.com>
2020-01-18 05:08:28 -0500

    express added, views are now ejs

---
+++ app.js
@@ -0,0 +1,18 @@
+const   express = require('express'),
+        HTTP_PORT = process.env.PORT || 8080,
+        app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('*', (req, res) => {
+
+})
+
+app.listen(HTTP_PORT, () => {
+    console.log('Server running on port: ' + HTTP_PORT);
+})
